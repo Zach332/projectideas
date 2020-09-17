7d7c0b2142d524469d1405b9bda13e54caa071fc
Zachary <heidelzachary@gmail.com>
2020-09-16 23:05:33 -0400

    Changed to use template Spring/React app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/projectideas-0.0.0.jar
---
+++ src/main/java/com/heroku/projectideas/SpringAndReactApplication.java
@@ -0,0 +1,12 @@
+package com.heroku.projectideas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndReactApplication.class, args);
+	}
+}
