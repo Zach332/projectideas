de916287a3871379b7ce83bf098cbad94059f5bb
Edward Noe <eln4jtt@virginia.edu>
2020-09-22 22:48:47 -0400

    Fix tabs in WebConfiguration.java

--- src/main/java/com/heroku/projectideas/WebConfiguration.java
+++ src/main/java/com/heroku/projectideas/WebConfiguration.java
@@ -7,14 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-  @Override
-  public void addViewControllers(ViewControllerRegistry registry) {
-      registry.addViewController("/{spring:\\w+}")
-            .setViewName("forward:/");
-      registry.addViewController("/**/{spring:\\w+}")
-            .setViewName("forward:/");
-      registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
-            .setViewName("forward:/");
-  }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+              .setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+}")
+              .setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+              .setViewName("forward:/");
+    }
 }
-
