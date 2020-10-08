75128424292caa939e81d897feb5d206de7a95ef
Zachary <heidelzachary@gmail.com>
2020-10-08 17:49:57 -0400

    Added styled brand to navbar

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import NameLogo from "../../namelogo.png"
 
 export default function Navbar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="/">projectideas</a>
+                <a className="navbar-brand" href="/">
+                    <img src={NameLogo} alt="" />
+                </a>
                 <button className="navbar-toggler mr-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
