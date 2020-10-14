6e224a51edc48e330069fcc7f284de36ad70283e
Edward Noe <eln4jtt@virginia.edu>
2020-10-13 22:51:21 -0400

    Add API endpoints

--- src/main/java/com/herokuapp/projectideas/database/DatabaseController.java
+++ src/main/java/com/herokuapp/projectideas/database/DatabaseController.java
@@ -1,12 +1,18 @@
 package com.herokuapp.projectideas.database;
 
+import java.util.Optional;
+
 import com.herokuapp.projectideas.database.documents.Post;
 import com.herokuapp.projectideas.database.documents.User;
 import com.herokuapp.projectideas.database.repositories.PostRepository;
 import com.herokuapp.projectideas.database.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,8 +37,23 @@ public class DatabaseController {
         return "Added post entry to database";
     }
 
-    @GetMapping("/api/posts")
+    @GetMapping("/api/posts/all")
     public Iterable<Post> getAllPosts() {
         return postRepository.findAll();
+    }
+
+    @GetMapping("/api/posts/{id}")
+    public Optional<Post> getPost(@PathVariable String id) {
+        return postRepository.findById(id);
+    }
+
+    @PostMapping("api/posts")
+    public void createPost(@RequestBody Post post) {
+        postRepository.save(post);
+    }
+
+    @DeleteMapping("/api/posts/{id}")
+    public void deletePost(@PathVariable String id) {
+        postRepository.deleteById(id);
     }
 }
