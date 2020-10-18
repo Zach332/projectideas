f386e0531d881df99218915fb5d56547cd726205
Edward Noe <eln4jtt@virginia.edu>
2020-10-17 22:11:03 -0400

    Rename Post to Idea

--- src/main/java/com/herokuapp/projectideas/database/documents/Idea.java
+++ src/main/java/com/herokuapp/projectideas/database/documents/Idea.java
@@ -6,15 +6,15 @@ import com.microsoft.azure.spring.data.cosmosdb.core.mapping.Document;
 
 import org.springframework.data.annotation.Id;
 
-@Document(collection = "Posts")
-public class Post {
+@Document(collection = "Ideas")
+public class Idea {
     
     @Id
     private String id;
     private String authorId;
     private String content;
 
-    public Post(String authorId, String content) {
+    public Idea(String authorId, String content) {
         this.id = UUID.randomUUID().toString();
         this.authorId = authorId;
         this.content = content;
--- src/main/java/com/herokuapp/projectideas/database/repositories/IdeaRepository.java
+++ src/main/java/com/herokuapp/projectideas/database/repositories/IdeaRepository.java
@@ -1,11 +1,11 @@
 package com.herokuapp.projectideas.database.repositories;
 
-import com.herokuapp.projectideas.database.documents.Post;
+import com.herokuapp.projectideas.database.documents.Idea;
 import com.microsoft.azure.spring.data.cosmosdb.repository.CosmosRepository;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends CosmosRepository<Post, String> {
+public interface IdeaRepository extends CosmosRepository<Idea, String> {
     
 }
