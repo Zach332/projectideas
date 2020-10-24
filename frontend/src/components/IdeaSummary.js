f20019c7273176a82fa92f90c7e55559aa74ce40
Zachary <heidelzachary@gmail.com>
2020-10-23 23:13:05 -0400

    Ideas now link to their pages

--- frontend/src/components/IdeaSummary.js
+++ frontend/src/components/IdeaSummary.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function IdeaSummary({idea}) {
+    var ideaLink = "/idea/"+idea.id
 
 	return (
-        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start m-3 rounded border">
+        <a href={ideaLink} class="list-group-item list-group-item-action flex-column align-items-start m-3 rounded border">
             <div class="d-flex w-100 justify-content-between">
                 <h5 class="mb-1">Title</h5>
                 <small class="text-muted">3 days ago</small>
