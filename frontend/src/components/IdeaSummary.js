109ecf422938faa6697ca1287df1b931ee2adc05
Zachary <heidelzachary@gmail.com>
2020-10-26 22:20:45 -0400

    Changed class -> className

--- frontend/src/components/IdeaSummary.js
+++ frontend/src/components/IdeaSummary.js
@@ -4,13 +4,13 @@ export default function IdeaSummary({idea}) {
     var ideaLink = "/idea/"+idea.id
 
 	return (
-        <a href={ideaLink} class="list-group-item list-group-item-action flex-column align-items-start m-3 rounded border">
-            <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1">{idea.title}</h5>
-                <small class="text-muted">3 days ago</small>
+        <a href={ideaLink} className="list-group-item list-group-item-action flex-column align-items-start m-3 rounded border">
+            <div className="d-flex w-100 justify-content-between">
+                <h5 className="mb-1">{idea.title}</h5>
+                <small className="text-muted">3 days ago</small>
             </div>
-            <p class="mb-1">{idea.content}</p>
-            <small class="text-muted">By {idea.authorUsername}</small>
+            <p className="mb-1">{idea.content}</p>
+            <small className="text-muted">By {idea.authorUsername}</small>
         </a>
 	);
 }
