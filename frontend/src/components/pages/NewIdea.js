29025d3f177720f741b3215b9c9b86e69d21d11d
Zachary <heidelzachary@gmail.com>
2020-10-26 23:34:10 -0400

    Submitted new idea form now submits the form to the database

--- frontend/src/components/pages/NewIdea.js
+++ frontend/src/components/pages/NewIdea.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import axios from 'axios'
+import CheckMark from "../../check.svg"
 
 export default function NewIdea() {
     const [idea, setIdea] = React.useState([{ title: '' , content: ''}])
+    const [submitted, setSubmitted] = React.useState(false)
 
     const handleInputChange = (event) => {
         const target = event.target;
@@ -15,23 +18,36 @@ export default function NewIdea() {
     }
 
     const handleSubmit = (event) => {
-        console.log(idea)
-        event.preventDefault();
+        axios.post("/api/ideas", {
+            authorUsername: 'Seg Fault',
+            title: idea.title,
+            content: idea.content
+        })
+        setSubmitted(true)
+        event.preventDefault()
     }
 
-    return (
-        <div className="mx-auto">
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="title">Title</label>
-                    <input type="text" className="form-control" id="title" onChange={handleInputChange}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="content">Details</label>
-                    <textarea className="form-control" id="content" rows="3" onChange={handleInputChange}></textarea>
-                </div>
-                <button type="submit" className="btn btn-primary">Post Idea</button>
-            </form>
-        </div>
-    )
+    if(!submitted) {
+        return (
+            <div className="mx-auto">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="title">Title</label>
+                        <input type="text" className="form-control" id="title" onChange={handleInputChange}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="content">Details</label>
+                        <textarea className="form-control" id="content" rows="3" onChange={handleInputChange}></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Post Idea</button>
+                </form>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <img src={CheckMark} class="mx-auto d-block pt-4" alt="Successful login" />
+            </div>
+        )
+    }
 }
