1ccc5c3a85d489c47ea946edf612fa95b8555e42
Zachary <heidelzachary@gmail.com>
2020-11-10 23:00:18 -0500

    Added markdown stripping for idea summaries

--- frontend/src/components/IdeaSummary.js
+++ frontend/src/components/IdeaSummary.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function IdeaSummary({idea}) {
+    const removeMd = require('remove-markdown');
     var ideaLink = "/idea/"+idea.id
     var date = new Date(idea.timePosted * 1000)
 
@@ -10,7 +11,7 @@ export default function IdeaSummary({idea}) {
                 <h5 className="mb-1">{idea.title}</h5>
                 <small className="text-muted">{date.toLocaleDateString()}</small>
             </div>
-            <p className="mb-1">{idea.content}</p>
+            <p className="mb-1">{removeMd(idea.content)}</p>
             <small className="text-muted">By {idea.authorUsername}</small>
         </a>
 	);
