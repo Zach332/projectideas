9afa6cceb7902bbb28d5d3ae5988746d19bcbcee
Zachary <heidelzachary@gmail.com>
2020-11-25 22:43:53 -0500

    Changed content for mobile devices where applicable

--- frontend/src/components/pages/NotFound.js
+++ frontend/src/components/pages/NotFound.js
@@ -2,14 +2,21 @@ import React from 'react'
 import ChromeDinoGame  from 'react-chrome-dino'
 
 export default function NotFound() {
+    const isMobile = window.innerWidth <= 768
     return (
         <div>
             <h1>Page Not Found</h1>
             <p>
-                The link you used does not appear to be valid. 
-                You can go to our <a href="/">home page</a>, or enjoy the game below (space to start/jump).
+                The link you used does not appear to be valid.
             </p>
-            <ChromeDinoGame />
+            {!isMobile &&
+                <div>
+                    <p>
+                        You can go to our <a href="/">home page</a>, or enjoy the game below (space to start/jump).
+                    </p>
+                    <ChromeDinoGame />
+                </div>
+            }
         </div>
     )
 }
