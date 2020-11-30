7ff50a0b6f8cde65d8f6381c082e28a4babc6c4c
Zachary <heidelzachary@gmail.com>
2020-11-29 23:13:29 -0500

    Made comment box grow and show submit button dynamically

--- frontend/src/components/Comments.js
+++ frontend/src/components/Comments.js
@@ -1,15 +1,23 @@
 import React from 'react'
 
 export default function Comments() {
+    const [rows, setRows] = React.useState(1)
+    const [showButton, setshowButton] = React.useState('hidden')
+
+    const onFocus = () => {
+        setRows(5)
+        setshowButton('visible')
+    }
+
     return (
         <div className="w-75 mt-5">
            <form>
-                <div class="form-row align-items-center">
+                <div class="form-row align-items-top">
                     <div class="col w-100">
-                        <textarea type="text" class="form-control mb-2" id="inlineFormInput" rows="1" placeholder="Write a comment "/>
+                        <textarea type="text" class="form-control mb-2" id="inlineFormInput" rows={rows} onFocus={onFocus} placeholder="Write a comment "/>
                     </div>
-                    <div class="col-auto">
-                        <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                    <div class="col-auto align-top">
+                        <button type="submit" class="btn btn-primary mb-2" style={{visibility:showButton}}>Submit</button>
                     </div>
                 </div>
             </form> 
