b19bceab95e70ba7b6e360cfd183860b6fef85de
Zachary <heidelzachary@gmail.com>
2020-11-30 23:13:01 -0500

    Don't allow non-authors to delete comments

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import axios from 'axios'
+import { useGlobalState } from '../State'
 import { useToasts } from 'react-toast-notifications'
 
 export default function Comment({comment, parentId, setRerender}) {
     const { addToast } = useToasts()
+    const [ user ] = useGlobalState('user')
     const deleteComment = () => {
         axios.delete("/api/ideas/"+parentId+"/comments/"+comment.id)
         .then(() => {
@@ -17,7 +19,8 @@ export default function Comment({comment, parentId, setRerender}) {
 
 	return (
         <div className="list-group-item flex-column align-items-start my-2 rounded border">
-            <div className="dropdown">
+            {user.username === comment.authorUsername &&
+                <div className="dropdown">
                     <button className="btn btn-outline-secondary float-right" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-three-dots-vertical" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
@@ -27,7 +30,7 @@ export default function Comment({comment, parentId, setRerender}) {
                         <a className="dropdown-item text-danger" onClick={deleteComment}>Delete comment</a>
                     </div>
                 </div>
-            
+            }
             <p className="mb-1">{comment.content}</p>
             <small className="text-muted">{comment.authorUsername}</small>
         </div>
