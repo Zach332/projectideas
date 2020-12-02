d94419a0efdcb95146b8e3813933d2ab88727144
Zachary <heidelzachary@gmail.com>
2020-12-01 22:39:09 -0500

    Make new comment box blank after submission; dynamically change box size in more circumstances

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -21,7 +21,7 @@ export default function Comment({comment, parentId, setRerender}) {
         <div className="list-group-item flex-column align-items-start my-2 rounded border">
             {(user.username === comment.authorUsername || user.isAdmin) &&
                 <div className="dropdown">
-                    <button className="btn btn-outline-secondary float-right" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <button className="btn btn-sm btn-outline-secondary float-right" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-three-dots-vertical" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
                         </svg>
