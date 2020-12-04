2893e54258aaf8dc096bd7109e0a737761ea0032
Edward Noe <eln4jtt@virginia.edu>
2020-12-03 22:24:28 -0500

    Clarify message not found error

--- src/main/java/com/herokuapp/projectideas/api/MessageController.java
+++ src/main/java/com/herokuapp/projectideas/api/MessageController.java
@@ -52,7 +52,7 @@ public class MessageController {
 
     private void markMessage(String recipientId, String messageId, boolean unread) {
         Message existingMessage = database.findMessageToUser(recipientId, messageId)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Message " + messageId + " does not exist."));
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Message " + messageId + " to user " + recipientId + " does not exist."));
         existingMessage.setUnread(unread);
         database.updateMessage(existingMessage);
     }
