0f37b6d1b680d54a7fdc24885a847dc04102aa91
Zachary <heidelzachary@gmail.com>
2020-12-10 22:51:32 -0500

    Enable quoting from button for non-admin users

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -59,7 +59,7 @@ export default function Comment({
             layout
             className="list-group-item flex-column align-items-start my-2 rounded border"
         >
-            {(user.username === comment.authorUsername || user.admin) && (
+            {user.username === comment.authorUsername || user.admin ? (
                 <div className="dropdown">
                     <button
                         className="btn btn-sm btn-outline-secondary float-right"
@@ -95,6 +95,39 @@ export default function Comment({
                             onClick={deleteComment}
                         >
                             Delete comment
+                        </a>
+                    </div>
+                </div>
+            ) : (
+                <div className="dropdown">
+                    <button
+                        className="btn btn-sm btn-outline-secondary float-right"
+                        type="button"
+                        id="dropdownMenuButton"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                    >
+                        <svg
+                            width="1em"
+                            height="1em"
+                            viewBox="0 0 16 16"
+                            className="bi bi-three-dots-vertical"
+                            fill="currentColor"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"
+                            />
+                        </svg>
+                    </button>
+                    <div
+                        className="dropdown-menu"
+                        aria-labelledby="dropdownMenuButton"
+                    >
+                        <a className="dropdown-item" onClick={quote}>
+                            Quote
                         </a>
                     </div>
                 </div>
