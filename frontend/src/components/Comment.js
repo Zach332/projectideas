cf5522549c624d87870eee56b58f5fd695229f8e
Zachary <heidelzachary@gmail.com>
2020-12-11 22:28:28 -0500

    Minor refactoring

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -59,79 +59,47 @@ export default function Comment({
             layout
             className="list-group-item flex-column align-items-start my-2 rounded border"
         >
-            {user.username === comment.authorUsername || user.admin ? (
-                <div className="dropdown">
-                    <button
-                        className="btn btn-sm btn-outline-secondary float-right"
-                        type="button"
-                        id="dropdownMenuButton"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
+            <div className="dropdown">
+                <button
+                    className="btn btn-sm btn-outline-secondary float-right"
+                    type="button"
+                    id="dropdownMenuButton"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                >
+                    <svg
+                        width="1em"
+                        height="1em"
+                        viewBox="0 0 16 16"
+                        className="bi bi-three-dots-vertical"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
                     >
-                        <svg
-                            width="1em"
-                            height="1em"
-                            viewBox="0 0 16 16"
-                            className="bi bi-three-dots-vertical"
-                            fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"
-                            />
-                        </svg>
-                    </button>
-                    <div
-                        className="dropdown-menu"
-                        aria-labelledby="dropdownMenuButton"
-                    >
-                        <a className="dropdown-item" onClick={quote}>
-                            Quote
-                        </a>
+                        <path
+                            fillRule="evenodd"
+                            d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"
+                        />
+                    </svg>
+                </button>
+                <div
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton"
+                >
+                    <a className="dropdown-item" onClick={quote}>
+                        Quote
+                    </a>
+                    {(user.username === comment.authorUsername ||
+                        user.admin) && (
                         <a
                             className="dropdown-item text-danger"
                             onClick={deleteComment}
                         >
                             Delete comment
                         </a>
-                    </div>
+                    )}
                 </div>
-            ) : (
-                <div className="dropdown">
-                    <button
-                        className="btn btn-sm btn-outline-secondary float-right"
-                        type="button"
-                        id="dropdownMenuButton"
-                        data-toggle="dropdown"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                    >
-                        <svg
-                            width="1em"
-                            height="1em"
-                            viewBox="0 0 16 16"
-                            className="bi bi-three-dots-vertical"
-                            fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"
-                            />
-                        </svg>
-                    </button>
-                    <div
-                        className="dropdown-menu"
-                        aria-labelledby="dropdownMenuButton"
-                    >
-                        <a className="dropdown-item" onClick={quote}>
-                            Quote
-                        </a>
-                    </div>
-                </div>
-            )}
+            </div>
             <p className="mb-1">
                 <ReactMarkdown
                     renderers={renderers}
