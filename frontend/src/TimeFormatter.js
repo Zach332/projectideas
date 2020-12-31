f3527bc25668e0735df5765ab9cad94cb184cc4c
Zachary <heidelzachary@gmail.com>
2020-12-30 22:14:43 -0500

    Additional date formatting improvements

--- frontend/src/TimeFormatter.js
+++ frontend/src/TimeFormatter.js
@@ -1,11 +1,13 @@
 export function formatTime(timeToFormat) {
     const DAY = 86400000;
     const timeSince = Date.now() - timeToFormat * 1000;
-    console.log(timeSince);
+    const dateToFormat = new Date(timeToFormat * 1000);
     if (timeSince < DAY) {
         return "Today";
     } else if (timeSince < DAY * 2) {
         return "Yesterday";
+    } else if (timeSince > DAY * 30) {
+        return dateToFormat.toLocaleDateString();
     } else {
         return Math.round(timeSince / DAY) + " days ago";
     }
