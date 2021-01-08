20d492936662b78a4aef2978798f5cc3526cfc37
Zachary <heidelzachary@gmail.com>
2021-01-07 23:00:50 -0500

    Fixed bugs and added search bar to search page

--- frontend/src/components/pages/Search.js
+++ frontend/src/components/pages/Search.js
@@ -7,13 +7,16 @@ import { toParams, toQuery } from "../utils/Routing";
 
 export default function Search() {
     const [ideas, setIdeas] = React.useState([]);
-    const [status, setStatus] = React.useState(Status.Loading);
+    const [status, setStatus] = React.useState(Status.NotSubmitted);
+    const [query, setQuery] = React.useState("");
     const [lastPage, setLastPage] = React.useState(true);
     const params = toParams(window.location.search.replace(/^\?/, ""));
     if (!params.page) params.page = 1;
 
     useEffect(() => {
         if (params.query) {
+            setQuery(params.query);
+            setStatus(Status.Loading);
             executeSearch();
         }
     }, []);
@@ -33,12 +36,24 @@ export default function Search() {
 
     const next = () => {
         window.location.href =
-            "/?" + toQuery({ page: parseInt(params.page) + 1 });
+            "/search?" +
+            toQuery({ query: query, page: parseInt(params.page) + 1 });
     };
 
     const previous = () => {
         window.location.href =
-            "/?" + toQuery({ page: parseInt(params.page) - 1 });
+            "/search?" +
+            toQuery({ query: query, page: parseInt(params.page) - 1 });
+    };
+
+    const handleInputChange = (event) => {
+        setQuery(event.target.value);
+        console.log("/search?" + toQuery({ query: query, page: 1 }));
+    };
+
+    const handleSubmit = () => {
+        window.location.href = "/search?" + toQuery({ query: query, page: 1 });
+        event.preventDefault();
     };
 
     let ideaElements;
@@ -54,12 +69,42 @@ export default function Search() {
         );
     } else if (status == Status.Loading) {
         ideaElements = <Spinner />;
-    } else {
+    } else if (!(status == Status.NotSubmitted)) {
         ideaElements = <p className="ms-2">No ideas match your search.</p>;
     }
 
     return (
         <div>
+            <form className="py-4" onSubmit={handleSubmit}>
+                <div className="row w-75 mx-auto">
+                    <div className="col me-auto">
+                        <input
+                            type="text"
+                            value={query}
+                            className="form-control"
+                            id="title"
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div className="col-auto my-auto">
+                        <button
+                            className="btn btn-sm btn-secondary"
+                            type="submit"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                fill="currentColor"
+                                className="bi bi-search"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </form>
             {ideaElements}
             <div className="d-flex">
                 <div className="me-auto p-2">
