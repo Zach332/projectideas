fcf93590ee4826346dc858b8189a443811600b36
Edward Noe <eln4jtt@virginia.edu>
2021-01-12 22:19:03 -0500

    Only show projects looking for members

--- src/main/java/com/herokuapp/projectideas/database/Database.java
+++ src/main/java/com/herokuapp/projectideas/database/Database.java
@@ -630,7 +630,7 @@ public class Database {
     public List<Project> getProjectsBasedOnIdea(String ideaId) {
         return projectContainer
             .queryItems(
-                "SELECT * FROM c WHERE c.type = 'Project' AND c.ideaId = '" +
+                "SELECT * FROM c WHERE c.type = 'Project' AND c.lookingForMembers = true AND c.ideaId = '" +
                 ideaId +
                 "'",
                 new CosmosQueryRequestOptions(),
