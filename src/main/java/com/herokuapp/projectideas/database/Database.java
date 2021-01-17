68d631c72f37b8842fff02cab6491768110d9fdf
Edward Noe <eln4jtt@virginia.edu>
2021-01-16 23:35:07 -0500

    Fix user join request username denormalization

--- src/main/java/com/herokuapp/projectideas/database/Database.java
+++ src/main/java/com/herokuapp/projectideas/database/Database.java
@@ -154,7 +154,12 @@ public class Database {
             // Handle projects container
             List<PartitionKey> projectPartitionKeys = projectContainer
                 .queryItems(
-                    "SELECT VALUE p.projectId FROM p JOIN u IN p.teamMembers WHERE u.userId = '" +
+                    "SELECT VALUE p.projectId FROM p " +
+                    "JOIN t IN p.teamMembers " +
+                    "JOIN r IN p.usersRequestingToJoin " +
+                    "WHERE t.userId = '" +
+                    user.getId() +
+                    "' OR r.userId = '" +
                     user.getId() +
                     "'",
                     new CosmosQueryRequestOptions(),
