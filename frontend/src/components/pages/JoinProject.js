b28e46e107ae870d0c9db8df59895bae59645b8f
Zachary <heidelzachary@gmail.com>
2021-01-18 23:07:37 -0500

    Show idea preview in join project page

--- frontend/src/components/pages/JoinProject.js
+++ frontend/src/components/pages/JoinProject.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Status } from "../../State";
+import NotFound from "./NotFound";
+import axios from "axios";
+import IdeaSummary from "./../IdeaSummary";
 
 export default function JoinProject() {
+    const [idea, setIdea] = React.useState([]);
+    const [status, setStatus] = React.useState(Status.Loading);
     let params = useParams();
 
-    return <div>{params.id}</div>;
+    useEffect(() => {
+        axios.get("/api/ideas/" + params.id).then((response) => {
+            if (!response.data) {
+                setStatus(Status.NotFound);
+            } else {
+                setIdea(response.data);
+            }
+        });
+    }, []);
+
+    if (status === Status.NotFound) {
+        return <NotFound />;
+    }
+
+    return (
+        <div>
+            <h1>Join or start a project for idea:</h1>
+            <div className="mx-3">
+                <IdeaSummary idea={idea} />
+            </div>
+        </div>
+    );
 }
