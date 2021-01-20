2dcf5d7a2d9679655250d63b6a54208d0ff94044
Zachary <heidelzachary@gmail.com>
2021-01-19 23:32:41 -0500

    Add request to join button to project summaries

--- frontend/src/components/ProjectSummary.js
+++ frontend/src/components/ProjectSummary.js
@@ -4,11 +4,23 @@ export default function ProjectSummary({ project }) {
     var ideaLink = "/project/" + project.id;
     const MAX_LENGTH = 480;
 
+    const toggleJoinRequest = (event) => {
+        event.preventDefault();
+    };
+
     return (
         <a
             href={ideaLink}
             className="list-group-item list-group-item-action flex-column align-items-start rounded border"
         >
+            <button
+                className="btn btn-sm btn-primary float-end"
+                type="button"
+                id="requestToJoin"
+                onClick={toggleJoinRequest}
+            >
+                Request to join
+            </button>
             <div className="d-flex justify-content-between">
                 <h5 className="mb-1">{project.name}</h5>
             </div>
