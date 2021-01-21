05d41701a779f47005069c44ac4d2063d1e3a744
Zachary <heidelzachary@gmail.com>
2021-01-20 23:19:11 -0500

    Show search on navbar even if not logged in

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -18,10 +18,9 @@ export default function Navbar() {
         });
     });
 
-    let login;
-    let userLinks;
+    let rightNavbar;
     if (user.loggedIn) {
-        userLinks = (
+        rightNavbar = (
             <ul className="navbar-nav ms-auto" id="navbarLogin">
                 <NavLink
                     className="nav-item nav-link me-3"
@@ -78,8 +77,24 @@ export default function Navbar() {
             </ul>
         );
     } else {
-        login = (
-            <div className="navbar-nav justify-content-end" id="navbarLogin">
+        rightNavbar = (
+            <ul className="navbar-nav ms-auto" id="navbarLogin">
+                <NavLink
+                    className="nav-item nav-link me-3"
+                    activeClassName="nav-item nav-link active"
+                    to="/search"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-search"
+                        viewBox="0 0 16 16"
+                    >
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                    </svg>
+                </NavLink>
                 <li className="navbar-btn">
                     <a
                         href="/login"
@@ -88,7 +103,7 @@ export default function Navbar() {
                         Login
                     </a>
                 </li>
-            </div>
+            </ul>
         );
     }
 
@@ -119,9 +134,8 @@ export default function Navbar() {
                             About
                         </NavLink>
                     </ul>
-                    {userLinks}
+                    {rightNavbar}
                 </div>
-                {login}
             </nav>
         </div>
     );
