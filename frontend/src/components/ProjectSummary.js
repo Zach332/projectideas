cc1e78c4cb64c0d47b68f27d0102fb4ff4d15a7a
Zachary <heidelzachary@gmail.com>
2021-01-20 22:43:37 -0500

    Only show request to join button in certain cases

--- frontend/src/components/ProjectSummary.js
+++ frontend/src/components/ProjectSummary.js
@@ -3,26 +3,45 @@ import React from "react";
 export default function ProjectSummary({ project }) {
     var ideaLink = "/project/" + project.id;
     const MAX_LENGTH = 480;
+    console.log(project);
 
-    const toggleJoinRequest = (event) => {
+    const sendJoinRequest = (event) => {
         event.preventDefault();
     };
+
+    var joinRequestButton = <div></div>;
+    if (project.userIsTeamMember) {
+        joinRequestButton = <div></div>;
+    } else if (project.userHasRequestedToJoin) {
+        joinRequestButton = (
+            <button
+                className="btn btn-sm btn-success float-end"
+                disabled={true}
+                type="button"
+                id="requestToJoin"
+            >
+                Join request sent
+            </button>
+        );
+    } else if (project.lookingForMembers) {
+        joinRequestButton = (
+            <button
+                className="btn btn-sm btn-primary float-end"
+                type="button"
+                id="requestToJoin"
+                onClick={sendJoinRequest}
+            >
+                Request to join
+            </button>
+        );
+    }
 
     return (
         <a
             href={ideaLink}
             className="list-group-item list-group-item-action flex-column align-items-start rounded border"
         >
-            {project.lookingForMembers && (
-                <button
-                    className="btn btn-sm btn-primary float-end"
-                    type="button"
-                    id="requestToJoin"
-                    onClick={toggleJoinRequest}
-                >
-                    Request to join
-                </button>
-            )}
+            {joinRequestButton}
             <div className="d-flex justify-content-between">
                 <h5 className="mb-1">{project.name}</h5>
             </div>
