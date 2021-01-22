f990603d40b7c91eeb33e5d5ee75f5f9c77b39ef
Zachary <heidelzachary@gmail.com>
2021-01-21 22:23:50 -0500

    Add button to join project from project page

--- frontend/src/components/pages/Project.js
+++ frontend/src/components/pages/Project.js
@@ -3,8 +3,10 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import NotFound from "./NotFound";
 import { Status } from "../../State";
+import { useToasts } from "react-toast-notifications";
 
 export default function Project() {
+    const { addToast } = useToasts();
     const [status, setStatus] = React.useState(Status.Loading);
     const [project, setProject] = React.useState({ teamMemberUsernames: [] });
     let params = useParams();
@@ -20,13 +22,66 @@ export default function Project() {
         });
     }, []);
 
+    const sendJoinRequest = (event) => {
+        axios
+            .post("/api/projects/" + project.id + "/joinrequests")
+            .then(() => {
+                project.userHasRequestedToJoin = true;
+                addToast("Your request was submitted.", {
+                    appearance: "success",
+                    autoDismiss: true,
+                });
+            })
+            .catch((err) => {
+                console.log("Error submitting request: " + err);
+                addToast("Your request was not submitted. Please try again.", {
+                    appearance: "error",
+                });
+            });
+        event.preventDefault();
+    };
+
+    var joinRequestButton = <div></div>;
+    if (project.userIsTeamMember) {
+        joinRequestButton = <div></div>;
+    } else if (project.userHasRequestedToJoin) {
+        joinRequestButton = (
+            <button
+                className="btn btn-md btn-success"
+                disabled={true}
+                type="button"
+                id="requestToJoin"
+            >
+                Join request sent
+            </button>
+        );
+    } else if (project.lookingForMembers) {
+        joinRequestButton = (
+            <button
+                className="btn btn-md btn-primary"
+                type="button"
+                id="requestToJoin"
+                onClick={sendJoinRequest}
+            >
+                Request to join
+            </button>
+        );
+    }
+
     if (status === Status.NotFound) {
         return <NotFound />;
     }
 
     return (
         <div>
-            <h1>{project.name}</h1>
+            <div className="d-flex">
+                <div className="me-auto p-2">
+                    <h1>{project.name}</h1>
+                </div>
+                <div className="d-flex align-items-center">
+                    {joinRequestButton}
+                </div>
+            </div>
             <p style={{ whiteSpace: "pre-wrap" }}>{project.description}</p>
             <table className="table">
                 <thead className="thead-dark">
