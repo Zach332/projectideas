1484a2dbfdf8d2efbd66510d364265d8e86b6c2e
Zachary <heidelzachary@gmail.com>
2021-01-27 22:36:31 -0500

    Autodismiss message success message

--- frontend/src/components/messageComponents/SendMessageModal.js
+++ frontend/src/components/messageComponents/SendMessageModal.js
@@ -38,6 +38,7 @@ export default function MessageModals({ recipient, id }) {
             .then(() => {
                 addToast("Your message was sent.", {
                     appearance: "success",
+                    autoDismiss: true,
                 });
                 setMessageToSend("");
             })
