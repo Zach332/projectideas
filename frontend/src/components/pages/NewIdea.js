8e319d644f66c2bb9e6c6e5951b23e3c0791296c
Zachary <heidelzachary@gmail.com>
2021-01-28 22:26:35 -0500

    Refactored tag code

--- frontend/src/components/pages/NewIdea.js
+++ frontend/src/components/pages/NewIdea.js
@@ -10,7 +10,7 @@ import {
 } from "../../State";
 import { useToasts } from "react-toast-notifications";
 import IdeaCard from "../ideaComponents/IdeaCard";
-import { toQuery } from "../utils/Routing";
+import TagPicker from "../tagComponents/TagPicker";
 
 export default function NewIdea() {
     const { addToast } = useToasts();
@@ -19,12 +19,10 @@ export default function NewIdea() {
         content: "",
         tags: [],
     });
-    const [tagSuggestions, setTagSuggestions] = React.useState([]);
-    const [newTag, setNewTag] = React.useState("");
+
     const [status, setStatus] = React.useState(Status.NotSubmitted);
     const [user] = useGlobalState("user");
     const [savedIdea] = useGlobalState("newIdea");
-    const [invalidCharacter, setInvalidCharacter] = React.useState(false);
 
     const handleInputChange = (event) => {
         const target = event.target;
@@ -35,65 +33,13 @@ export default function NewIdea() {
         }));
     };
 
-    const filterTagSuggestions = (suggestions) => {
-        return suggestions.filter((tag) => !idea.tags.includes(tag));
-    };
-
     useEffect(() => {
         localStorage.setItem(newIdeaPersistenceKey, JSON.stringify(idea));
     }, [idea]);
 
     useEffect(() => {
         setIdea(savedIdea);
-        axios.get("/api/tags/standard/idea").then((response) => {
-            setTagSuggestions(response.data);
-        });
     }, []);
-
-    const addTag = (tagName) => {
-        if (idea.tags.length < 5 && tagName.length > 0 && tagName.length < 30) {
-            setIdea((idea) => ({
-                ...idea,
-                tags: idea.tags.includes(tagName)
-                    ? idea.tags
-                    : idea.tags.concat(tagName),
-            }));
-            setNewTag("");
-            axios.get("/api/tags/standard/idea").then((response) => {
-                setTagSuggestions(response.data);
-            });
-        }
-    };
-
-    const removeTag = (tagName) => {
-        setIdea((idea) => ({
-            ...idea,
-            tags: idea.tags.filter((tag) => tag != tagName),
-        }));
-    };
-
-    const updateNewTag = (event) => {
-        setNewTag(event.target.value);
-        setInvalidCharacter(false);
-        if (event.target.value === "") {
-            axios.get("/api/tags/standard/idea").then((response) => {
-                setTagSuggestions(response.data);
-            });
-        } else {
-            axios
-                .get(
-                    "/api/tags/suggested/idea?" +
-                        toQuery({ search: event.target.value })
-                )
-                .then((response) => {
-                    setTagSuggestions(response.data);
-                });
-        }
-    };
-
-    const createTag = () => {
-        addTag(newTag);
-    };
 
     const handleSubmit = (event) => {
         axios
@@ -115,25 +61,6 @@ export default function NewIdea() {
                 });
             });
         event.preventDefault();
-    };
-
-    const validateTagKeyPress = (event) => {
-        // enter key
-        if (event.keyCode === 13) {
-            createTag();
-            event.preventDefault();
-        } else if (
-            (event.key >= "a" && event.key <= "z") ||
-            event.key === "-"
-        ) {
-            return;
-        } else if (
-            (event.keyCode >= 58 && event.keyCode <= 90) ||
-            (event.keyCode >= 160 && event.keyCode <= 165)
-        ) {
-            event.preventDefault();
-            setInvalidCharacter(true);
-        }
     };
 
     if (!user.loggedIn) {
@@ -164,72 +91,7 @@ export default function NewIdea() {
                             onChange={handleInputChange}
                         ></textarea>
                     </div>
-                    {idea.tags && (
-                        <div>
-                            {idea.tags.map((tag) => (
-                                <span
-                                    className="badge btn rounded-pill bg-dark me-2"
-                                    onClick={() => removeTag(tag)}
-                                    key={tag}
-                                >
-                                    {tag}{" "}
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="16"
-                                        height="16"
-                                        fill="currentColor"
-                                        className="bi bi-x"
-                                        viewBox="0 0 16 16"
-                                    >
-                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                                    </svg>
-                                </span>
-                            ))}
-                            {idea.tags.length > 0 && idea.tags.length + "/" + 5}
-                        </div>
-                    )}
-                    <div className="mt-1">
-                        <br></br>
-                    </div>
-                    <form className="row g-3">
-                        <div className="col-auto">
-                            <span>Add tag</span>
-                        </div>
-                        <div className="col-auto">
-                            <input
-                                type="text"
-                                className="form-control"
-                                onKeyDown={validateTagKeyPress}
-                                value={newTag}
-                                onChange={updateNewTag}
-                                placeholder="new tag"
-                            />
-                        </div>
-                        <div className="col-auto">
-                            <div
-                                className="btn btn-primary mb-3"
-                                onClick={createTag}
-                            >
-                                Add
-                            </div>
-                        </div>
-                        {(newTag.length > 29 || invalidCharacter) && (
-                            <div className="col-auto text-red">
-                                Tags must be less than 30 characters, and
-                                include only lowercase letters, numbers, or
-                                dashes
-                            </div>
-                        )}
-                    </form>
-                    {filterTagSuggestions(tagSuggestions).map((tag) => (
-                        <span
-                            className="badge btn rounded-pill bg-secondary me-2"
-                            onClick={() => addTag(tag)}
-                            key={tag}
-                        >
-                            {tag}
-                        </span>
-                    ))}
+                    <TagPicker post={idea} setPost={setIdea} postType="idea" />
                     <br></br>
                     <button
                         type="submit"
