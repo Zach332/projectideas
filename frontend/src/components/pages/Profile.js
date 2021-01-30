a20c3767bd9291f63942e758f5c542ba79c83b31
Zachary <heidelzachary@gmail.com>
2021-01-30 03:56:33 -0500

    Fixed incorrect section naming

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -206,9 +206,9 @@ export default function Profile() {
             >
                 Log Out
             </button>
-            <h2 className="mt-4">Saved projects</h2>
+            <h2 className="mt-4">Saved ideas</h2>
             {myProjects.length === 0 ? (
-                <p>You haven&apos;t saved any projects.</p>
+                <p>You haven&apos;t saved any ideas.</p>
             ) : (
                 myProjects.map((idea) => (
                     <div key={idea.id} className="container-flex">
