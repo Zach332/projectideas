512e6ff8fb9fd3ade1b7238eecc356ca0a074268
Zachary <heidelzachary@gmail.com>
2021-02-11 22:38:33 -0500

    Improve coloring of quotes in comments

--- frontend/src/components/commentComponents/Comment.js
+++ frontend/src/components/commentComponents/Comment.js
@@ -18,9 +18,8 @@ export default function Comment({
             text.startsWith(">") ? (
                 <div
                     key={Math.floor(Math.random() * 1000000000)}
-                    className="mx-5 ps-2"
+                    className="mx-5 ps-2 bg-light"
                     style={{
-                        background: "#ededed",
                         wordBreak: "break-word",
                     }}
                 >
