02e32085e488e6f363328a130f28a503d1f65f55
Zachary <heidelzachary@gmail.com>
2021-02-17 23:01:19 -0500

    Add paging to My Projects, Saved Ideas, and My Ideas

--- frontend/src/components/pages/MyProjects.js
+++ frontend/src/components/pages/MyProjects.js
@@ -7,18 +7,35 @@ import LoadingDiv from "./../general/LoadingDiv";
 import { Helmet } from "react-helmet";
 import { Globals } from "../../GlobalData";
 import LoginWarning from "./../logins/LoginWarning";
+import { toQuery } from "../utils/Routing";
 
 export default function MyProjects({ noHeading }) {
     const [projects, setProjects] = React.useState([]);
     const [status, setStatus] = React.useState(Status.Loading);
     const [user] = useGlobalState("user");
+    const [page, setPage] = React.useState(1);
+    const [lastPage, setLastPage] = React.useState(true);
 
     useEffect(() => {
-        axios.get("/api/users/" + user.id + "/projects").then((response) => {
-            setProjects(response.data);
-            setStatus(Status.Loaded);
-        });
-    }, []);
+        setStatus(Status.Loading);
+        axios
+            .get(
+                "/api/users/" + user.id + "/projects?" + toQuery({ page: page })
+            )
+            .then((response) => {
+                setProjects(response.data.projectPreviews);
+                setLastPage(response.data.lastPage);
+                setStatus(Status.Loaded);
+            });
+    }, [page]);
+
+    const next = () => {
+        setPage((page) => page + 1);
+    };
+
+    const previous = () => {
+        setPage((page) => page - 1);
+    };
 
     const existingProjects =
         projects.length > 0 ? (
@@ -51,6 +68,30 @@ export default function MyProjects({ noHeading }) {
             )}
             {!noHeading && <h1>My Projects</h1>}
             {existingProjects}
+            <div className="d-flex">
+                <div className="me-auto p-2">
+                    {page > 1 && (
+                        <button
+                            type="btn btn-primary"
+                            className="btn btn-primary btn-md"
+                            onClick={previous}
+                        >
+                            Previous
+                        </button>
+                    )}
+                </div>
+                <div className="p-2">
+                    {!lastPage && projects.length > 0 && (
+                        <button
+                            type="btn btn-primary"
+                            className="btn btn-primary btn-md"
+                            onClick={next}
+                        >
+                            Next
+                        </button>
+                    )}
+                </div>
+            </div>
         </LoadingDiv>
     );
 }
