3d0be1c0d725f142b16627d34e8261a2ec6a3e35
Zachary <heidelzachary@gmail.com>
2021-04-06 22:26:06 -0400

    Dismiss modal after clicking 'Login here'

--- frontend/src/components/logins/LoginWarning.js
+++ frontend/src/components/logins/LoginWarning.js
@@ -1,14 +1,25 @@
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export default function LoginWarning() {
+    let history = useHistory();
+
+    const onClick = () => {
+        history.push("/login");
+    };
+
     return (
         <div className="p-4 bg-light">
             <h1 className="display-4">Login required</h1>
             <p className="lead">You must login to view this page.</p>
             <hr className="my-4" />
-            <Link className="btn btn-primary btn-lg" to="/login" role="button">
+            <button
+                className="btn btn-primary btn-lg"
+                data-bs-dismiss="modal"
+                onClick={onClick}
+                role="button"
+            >
                 Login here
-            </Link>
+            </button>
         </div>
     );
 }
