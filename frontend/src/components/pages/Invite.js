8c38a149a4e0ce44580702e091367a89b17b6da0
Zachary <heidelzachary@gmail.com>
2021-04-19 23:23:06 -0400

    Add invite page for accepting project invites

---
+++ frontend/src/components/pages/Invite.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Status } from "../../State";
+import Spinner from "../general/Spinner";
+import Success from "../general/Success";
+import XMark from "../../x.svg";
+import { toParams } from "../utils/Routing";
+
+export default function Invite() {
+    const [status, setStatus] = useState(Status.Loading);
+    const [message, setMessage] = useState("");
+    const [projectId, setProjectId] = useState("");
+    const params = toParams(location.search.replace(/^\?/, ""));
+
+    useEffect(() => {
+        axios
+            .post("/api/projects/invites/" + params.id + "/accept")
+            .then((response) => {
+                setProjectId(response.data);
+                setStatus(Status.Success);
+                setMessage(
+                    "You were successfully added to the project. Go to your new project"
+                );
+            })
+            .catch((err) => {
+                if (err.response.status == 404) {
+                    setStatus(Status.Failure);
+                    setMessage(
+                        "The invite link you used is not valid. Please ensure your invite link is up-to-date for the project you are trying to join."
+                    );
+                } else {
+                    setStatus(Status.Failure);
+                    setMessage(
+                        "We were unable to add you to the project. Please ensure that you are logged in and not already a member of the project."
+                    );
+                }
+            });
+    }, []);
+
+    if (status === Status.Success) {
+        return (
+            <div>
+                <Success link={"/project/" + projectId} message={message} />
+            </div>
+        );
+    }
+
+    if (status == Status.Failure) {
+        return (
+            <div className="text-center">
+                <img
+                    src={XMark}
+                    className="mx-auto w-25 d-block py-4"
+                    alt="Invite failed"
+                />
+                <p>{message}</p>
+            </div>
+        );
+    }
+
+    return <Spinner />;
+}
