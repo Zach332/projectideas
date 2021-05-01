54dc838a9fc3ebc30efc93533563678509144662
Edward Noe <eln4jtt@virginia.edu>
2021-04-30 23:26:43 -0400

    Change notification preference API to take DTO

--- src/main/java/com/herokuapp/projectideas/api/EmailController.java
+++ src/main/java/com/herokuapp/projectideas/api/EmailController.java
@@ -3,6 +3,7 @@ package com.herokuapp.projectideas.api;
 import com.herokuapp.projectideas.database.Database;
 import com.herokuapp.projectideas.database.document.user.NotificationPreference;
 import com.herokuapp.projectideas.database.exception.EmptySingleDocumentQueryException;
+import com.herokuapp.projectideas.dto.email.UpdateNotificationPreferenceDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,12 +21,12 @@ public class EmailController {
     @PostMapping("/api/email/notificationPreference/{emailSubscriptionId}")
     public void updateEmailNotificationPreference(
         @PathVariable String emailSubscriptionId,
-        @RequestBody NotificationPreference notificationPreference
+        @RequestBody UpdateNotificationPreferenceDTO notificationPreferenceDTO
     ) {
         try {
             database.updateEmailNotificationPreference(
                 emailSubscriptionId,
-                notificationPreference
+                notificationPreferenceDTO.getNotificationPreference()
             );
         } catch (EmptySingleDocumentQueryException e) {
             throw new ResponseStatusException(
