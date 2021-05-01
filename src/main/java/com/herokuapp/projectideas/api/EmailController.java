e5565e95bef11b57bbba434e8c1f6757d0bef415
Edward Noe <eln4jtt@virginia.edu>
2021-04-30 22:29:40 -0400

    Add API to change notification preference from subscription id

--- src/main/java/com/herokuapp/projectideas/api/EmailController.java
+++ src/main/java/com/herokuapp/projectideas/api/EmailController.java
@@ -1,11 +1,13 @@
 package com.herokuapp.projectideas.api;
 
 import com.herokuapp.projectideas.database.Database;
+import com.herokuapp.projectideas.database.document.user.NotificationPreference;
 import com.herokuapp.projectideas.database.exception.EmptySingleDocumentQueryException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,16 +17,20 @@ public class EmailController {
     @Autowired
     Database database;
 
-    @PostMapping("/api/email/unsubscribe/{unsubscribeId}")
-    public void unsubscribeFromEmailNotifications(
-        @PathVariable String unsubscribeId
+    @PostMapping("/api/email/notificationPreference/{emailSubscriptionId}")
+    public void updateEmailNotificationPreference(
+        @PathVariable String emailSubscriptionId,
+        @RequestBody NotificationPreference notificationPreference
     ) {
         try {
-            database.unsubscribeFromEmailNotifications(unsubscribeId);
+            database.updateEmailNotificationPreference(
+                emailSubscriptionId,
+                notificationPreference
+            );
         } catch (EmptySingleDocumentQueryException e) {
             throw new ResponseStatusException(
                 HttpStatus.NOT_FOUND,
-                "Unsubscribe id " + unsubscribeId + " does not exist."
+                "Subscription id " + emailSubscriptionId + " does not exist."
             );
         }
     }
