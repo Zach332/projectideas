568d05bd1e3e4824d0646dfbc980f909921687ae
Edward Noe <eln4jtt@virginia.edu>
2021-05-03 22:35:42 -0400

    Remove TODO

--- src/main/java/com/herokuapp/projectideas/database/document/Authorization.java
+++ src/main/java/com/herokuapp/projectideas/database/document/Authorization.java
@@ -1,6 +1,5 @@
 package com.herokuapp.projectideas.database.document;
 
-// TODO: move interfaces to their own directory
 public interface Authorization {
     boolean userIsAuthorizedToView(String userId);
     boolean userIsAuthorizedToEdit(String userId);
