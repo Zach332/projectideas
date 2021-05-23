c2ef54bcd5e535947dcb51976e9c66c7812a237b
Edward Noe <eln4jtt@virginia.edu>
2021-05-22 23:32:34 -0400

    Remove test CrossOrigin annotations

--- src/main/java/com/herokuapp/projectideas/api/ProjectController.java
+++ src/main/java/com/herokuapp/projectideas/api/ProjectController.java
@@ -18,7 +18,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,7 +31,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@CrossOrigin
 public class ProjectController {
 
     @Autowired
