8016f4d063946d5cc95a0de30aae745fcf73dd08
Edward Noe <eln4jtt@virginia.edu>
2021-05-24 02:12:16 -0400

    Add DevCorsConfig

--- src/main/java/com/herokuapp/projectideas/config/ProdCorsConfig.java
+++ src/main/java/com/herokuapp/projectideas/config/ProdCorsConfig.java
@@ -1,19 +1,20 @@
 package com.herokuapp.projectideas.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
+@Profile("prod")
+public class ProdCorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/api/**")
-            .allowCredentials(true)
             .allowedMethods("*")
             .allowedOrigins("https://projectideas.tech");
     }
