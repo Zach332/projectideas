4ece12a60c2f68788bb74e13129869d24a388791
Edward Noe <eln4jtt@virginia.edu>
2021-05-26 01:08:04 -0400

    Change default notification setting - Send email if user has not read the new message after five minutes - Add NotificationService.java

---
+++ src/main/java/com/herokuapp/projectideas/notification/NotificationService.java
@@ -0,0 +1,60 @@
+package com.herokuapp.projectideas.notification;
+
+import com.herokuapp.projectideas.database.Database;
+import com.herokuapp.projectideas.database.document.user.User;
+import com.herokuapp.projectideas.database.exception.EmptyPointReadException;
+import com.herokuapp.projectideas.email.EmailInterface;
+import java.time.Instant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service for sending notification emails. Waits before sending an email.
+ */
+@Service
+public class NotificationService {
+
+    @Autowired
+    Database database;
+
+    @Autowired
+    EmailInterface emailInterface;
+
+    private static final int MILLISECONDS_BEFORE_SENDING_EMAIL = 1000 * 60 * 5;
+
+    @Async
+    public void notifyUserOfUnreadMessages(User user)
+        throws EmptyPointReadException {
+        int numUnreadMessages = user.getUnreadMessages();
+        if (numUnreadMessages > 0) {
+            boolean sendEmail =
+                switch (user.getNotificationPreference()) {
+                    // Only send an email if the user is up-to-date with reading messages
+                    case Default -> numUnreadMessages == 1;
+                    case AllNewMessages -> true;
+                    case Unsubscribed -> false;
+                };
+
+            if (sendEmail) {
+                // Wait before sending email in case the user has checked their messages
+                // since the start of this method
+                try {
+                    Thread.sleep(MILLISECONDS_BEFORE_SENDING_EMAIL);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                user = database.getUser(user.getUserId());
+                numUnreadMessages = user.getUnreadMessages();
+                if (numUnreadMessages == 0) {
+                    return;
+                }
+
+                emailInterface.sendUnreadMessagesEmail(user, numUnreadMessages);
+
+                user.setTimeLastEmailReceived(Instant.now().getEpochSecond());
+                database.updateUser(user.getUserId(), user);
+            }
+        }
+    }
+}
